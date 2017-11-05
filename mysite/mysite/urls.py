84ca03dbd95cb8a95e6e05809dfb9fce8d5a345e
Ian Williams <ian201623@outlook.com>
2017-11-06 08:34:24 +1000

    added hours ahead function

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from mysite.views import hello
+from mysite.views import hello, current_datetime, hours_ahead
 # admin.autodiscover()
 
 urlpatterns = [
@@ -15,9 +15,11 @@ urlpatterns = [
 # url(r'^mysite/', include('mysite.mysite.urls')),
 
 # Uncomment the admin/doc line below to enable admin documentation:
-# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 # Uncomment the next line to enable the admin:
-# url(r'^admin/', include(admin.site.urls)),
+url(r'^admin/', include(admin.site.urls)),
 url(r'^hello/$', hello),
+url(r'^time/$', current_datetime),
+url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
